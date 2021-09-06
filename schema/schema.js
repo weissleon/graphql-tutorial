9dfcdcc7310ecc45facaf27cb39779a6e32eb32c
weissleon <weissleon1943@gmail.com>
2021-09-06 23:15:17 +0900

    Update content

--- schema/schema.js
+++ schema/schema.js
@@ -1,14 +1,29 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+} = graphql;
 
-// This is whre we create the required types.
+// This is where we create the required types.
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -18,20 +33,34 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       // The function below will be used to connect to the db of our choice.
       resolve(parent, args) {
         // code to get data from db / other source
         return books.find((book) => book.id == args.id);
       },
     },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors.find((authors) => authors.id == args.id);
+      },
+    },
   },
 });
 
+// Dummy Data
 const books = [
   { name: "Name of the Wind", genre: "Fantasy", id: "1" },
   { name: "The Final Empire", genre: "Fantasy", id: "2" },
   { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+];
+
+const authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" },
 ];
 
 module.exports = new GraphQLSchema({
